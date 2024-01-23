b7531ab5561573c6c7e9b0bd41ca188461193edf
Harbinger of Fire <harbingeroffire@proton.me>
2024-01-23 15:23:57 -0700

    final

--- SXH.py
+++ SXH.py
@@ -1,14 +1,23 @@
+# SXH.py
 class New:
     def __init__(self, _algorithm, _buffer):
         try:
             algorithm_module = __import__(_algorithm.upper())
             algorithm_class = getattr(algorithm_module, _algorithm)
             
-            algorithm_class(_buffer)
+            self.algorithm_instance = algorithm_class(_buffer)
         except ImportError:
             print(f"Error: Algorithm {_algorithm} not found.")
         except AttributeError:
             print(f"Error: Class {_algorithm} not found in the {_algorithm.lower()} module.")
 
+    def hash(self):
+        if hasattr(self, 'algorithm_instance'):
+            return self.algorithm_instance.hash()
+        else:
+            print("Error: Algorithm instance not found.")
+
 if __name__ == "__main__":
-    New("SXH128", b"Hello, World!")
+    print(New("SXH128", b"Hello, World!"*100).hash(), end="\n\n")
+    print(New("SXH256", b"Hello, World!"*100).hash(), end="\n\n")
+    print(New("SXH512", b"Hello, World!"*100).hash(), end="\n\n")
--- SXH128.py
+++ SXH128.py
@@ -1,3 +1,38 @@
 class SXH128:
     def __init__(self, _buffer):
-        self.hash(_buffer)
+        self.SIZE = 128
+        self._buffer = _buffer
+    
+    def block(self, _buffer):
+        return [_buffer[i:i+self.SIZE] for i in range(0, len(_buffer), self.SIZE)]
+
+    def modulate(self, _buffer: bytes):
+        _new_buffer=b""
+        for byte in _buffer:
+            byte=byte%16
+            _new_buffer+=hex(byte).replace("0x", "").encode()
+        return _new_buffer
+
+    def xor_chars(self, _buffer: bytes):
+        _buffer=self.modulate(_buffer)
+        result = ''
+        for i in range(len(_buffer) - 1):
+            xor_result = _buffer[i] ^ _buffer[i + 1]
+            result += format(xor_result, '01x') 
+        return result
+    
+    def xor_strings(self, _buffer: list[str]):
+        for i in range(len(_buffer) - 1):
+            xor_result = ""
+            for j in range(self.SIZE):
+                xor_result += hex((int(_buffer[0][j], 16) ^ int(_buffer[i + 1][j], 16)) % 16)[2:]
+            _buffer[0] = xor_result
+        return _buffer[0]
+    
+    def hash(self):
+        blocks=[]
+        for block in self.block(self._buffer):
+            blocks.append(self.xor_chars(block.ljust(self.SIZE, b"\0")))
+        result=self.xor_strings(blocks)        
+        return result
+        
